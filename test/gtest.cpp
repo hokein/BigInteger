1b1302c5574b58d2c0407c06f6fd224b9359f99c
Haojian Wu <hokein.wu@gmail.com>
2013-10-21 18:24:49 -0700

    Fix a include head file error.

--- test/gtest.cpp
+++ test/gtest.cpp
@@ -1,12 +1,12 @@
 #include <gtest/gtest.h>
-#include "../BigInteger.h"
+#include "../src/BigInteger.h"
 
 TEST(BigIntegerTest, Plus) {
   BigInteger a[] = {"0", "0", "0", "9", "999", "100", "111111"};
   BigInteger b[] = {"0", "1", "9999", "1", "1", "100000", "90"};
   BigInteger expected_res[] = {"0", "1", "9999", "10", "1000", "100100",
       "111201"};
-  
+
   for (int i = 0; i < sizeof(expected_res)/sizeof(BigInteger); ++i) {
     EXPECT_EQ(expected_res[i], a[i]+b[i]);
     EXPECT_EQ(expected_res[i], b[i]+a[i]);
@@ -18,7 +18,7 @@ TEST(BigIntegerTest, Substract) {
   BigInteger b[] = {"0", "0", "1", "1", "1", "9", "11"};
   BigInteger expected_res[] = {"0", "1", "0", "8", "99", "990",
       "99999999999999999989"};
-  
+
   for (int i = 0; i < sizeof(expected_res)/sizeof(BigInteger); ++i) {
     EXPECT_EQ(expected_res[i], a[i]-b[i]);
   }
@@ -31,7 +31,7 @@ TEST(BigIntegerTest, Multiply) {
       "99999999999999999999"};
   BigInteger expected_res[] = {"0", "0", "1", "9", "900", "8991",
       "1100000000000000000000", "9999999999999999999800000000000000000001"};
-  
+
   for (int i = 0; i < sizeof(expected_res)/sizeof(BigInteger); ++i) {
     EXPECT_EQ(expected_res[i], a[i]*b[i]);
     EXPECT_EQ(expected_res[i], b[i]*a[i]);
@@ -45,7 +45,7 @@ TEST(BigIntegerTest, Divide) {
       "10000000000000000000", "99999999999999999999"};
   BigInteger expected_res[] = {"0", "0", "1111", "2", "1", "9", "4",
       "110", "99999999999999999999"};
-  
+
   for (int i = 0; i < sizeof(expected_res)/sizeof(BigInteger); ++i) {
     EXPECT_EQ(expected_res[i], a[i]/b[i]);
   }
